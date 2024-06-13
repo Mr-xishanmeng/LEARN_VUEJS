864a651bb62592da64a0ac53ce8cb2b11428d73e
xishanmeng <920955414@qq.com>
2024-06-13 19:27:31 +0800

    组件插槽

---
+++ 03_learn_components/src/05_组件插槽-基本使用/ShowMessage.vue
@@ -0,0 +1,21 @@
+<template>
+  <h2>{{ title }}</h2>
+  <div class="content">
+    <slot>
+      <p>我是插槽slot的默认值</p>
+    </slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      title: {
+        type: String,
+        default: '我是title的默认值',
+      },
+    },
+  };
+</script>
+
+<style scoped></style>
